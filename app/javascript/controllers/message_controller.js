2f31a288c70c6dda12f231ce134f2d92db34df57
APS <anirudhapsah@gmail.com>
2024-03-12 16:29:59 +0530

    remove event listener

--- app/javascript/controllers/message_controller.js
+++ app/javascript/controllers/message_controller.js
@@ -14,12 +14,20 @@ export default class extends Controller {
   }
   handleFocus() {
     const messageDiv = this.element
-    this.element.addEventListener('keydown', (e) => {
+
+    const handlekeydown = (e) => {
       const triggerRect = messageDiv.getBoundingClientRect();
       if (e.key == "Enter") {
         this.application.getControllerForElementAndIdentifier(this.popupTarget, 'popup').show({clientX: triggerRect.left + triggerRect.width - 28, clientY: triggerRect.top + 16})
+        removeListener()
       }
-    })
+    }
+    const removeListener = () => {
+      this.element.removeEventListener('keydown', handlekeydown)
+    }
+
+    this.element.addEventListener('keydown', handlekeydown)
+
   }
   hidePopup() {
     this.application.getControllerForElementAndIdentifier(this.popupTarget, 'popup').hide()
