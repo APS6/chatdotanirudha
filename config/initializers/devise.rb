45c65f13ebd737469a6ac325c5885f6bc398bd08
APS <anirudhapsah@gmail.com>
2024-05-11 19:42:14 +0530

    use rails credentials for google oauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :google_oauth2, '1073544784519-dg7fi37gg6arglp2b5m9geo021sab0ku.apps.googleusercontent.com', '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', {}
+  config.omniauth :google_oauth2, Rails.application.credentials.dig(:google_oauth, :client_id), Rails.application.credentials.dig(:google_oauth, :client_secret), {}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
