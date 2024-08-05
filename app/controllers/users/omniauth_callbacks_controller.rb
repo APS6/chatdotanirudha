9396e57fff3f8f4595bd7619aff064c0a8f87570
APS <anirudhapsah@gmail.com>
2024-08-05 15:49:47 +0530

    add remember me on google sign in

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,12 +3,14 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
+    include Devise::Controllers::Rememberable
 
   def google_oauth2
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
       if @user.persisted?
         sign_in @user, event: :authentication
+        remember_me(@user)
         redirect_to root_path, notice: "Logged in as #{@user.username}"
       else
         session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
